5a8eec8bb4cc5b7857295694502af8f9de6116a8
Arsen <arsproger@gmail.com>
2023-05-12 22:25:37 +0600

    init

---
+++ src/main/java/com/example/springbootherokustarter/SpringBootHerokuStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootherokustarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHerokuStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHerokuStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootherokustarter/SpringBootHerokuStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootherokustarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHerokuStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
